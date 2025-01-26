863fe2190fe75a11047742a6d8d3a9dc01b79a01
Maguire Ellsworth <m3k4e@byu.edu>
2025-01-25 20:34:18 -0700

    updated ChessBoard equals and hashcode - passed ChessBoardTests

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -38,6 +41,62 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        ChessPiece.PieceType[] lineUp = {
+                ChessPiece.PieceType.ROOK,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.KING,
+                ChessPiece.PieceType.BISHOP,
+                ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.ROOK
+        };
+        //add white pieces
+        for(int i = 0; i <= 7; i++){
+            addPiece(new ChessPosition(1, i + 1), new ChessPiece(ChessGame.TeamColor.WHITE, lineUp[i]));
+            addPiece((new ChessPosition(2, i + 1)), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+        }
+        //add black pieces
+        for(int i = 0; i <= 7; i++){
+            addPiece(new ChessPosition(8, i + 1), new ChessPiece(ChessGame.TeamColor.BLACK, lineUp[i]));
+            addPiece((new ChessPosition(7, i + 1)), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        for(int i = 0; i < spaces.length; i++){
+            for(int j = 0; j < spaces[i].length; j++){
+                ChessPiece myPiece = spaces[i][j];
+                ChessPiece compPiece = that.spaces[i][j];
+                if(myPiece == null && compPiece == null){
+                    break;
+                }
+                if((myPiece == null && compPiece != null) || (myPiece != null && compPiece == null)){
+                    return false;
+                }
+                if(myPiece.getPieceType() != compPiece.getPieceType()
+                        || myPiece.getTeamColor() != compPiece.getTeamColor()){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(spaces);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "spaces=" + Arrays.toString(spaces) +
+                '}';
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -68,5 +69,19 @@ public class ChessPiece {
             default:
                 return new ArrayList<>();
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && piecetype == that.piecetype;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, piecetype);
     }
 }
