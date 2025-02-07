6300c7d57cebd8b71c7ef7f415c7ad572722726e
Maguire Ellsworth <m3k4e@byu.edu>
2025-02-06 19:11:20 -0700

    implemented isInStalemate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -204,6 +204,9 @@ public class ChessGame {
      */
     public boolean isInStalemate(TeamColor teamColor) {
         Collection<ChessMove> allValidMoves = new ArrayList<>();
+        if(isInCheck(teamColor)){
+            return false;
+        }
         for(int i= 1; i <= 8; i++){
             for(int j = 1; j <= 8; j++){
                 ChessPosition position = new ChessPosition(i, j);
