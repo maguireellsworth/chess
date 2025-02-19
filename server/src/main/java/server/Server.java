a6ef80c9ef58c327e37778a321d2869135ac5797
Maguire Ellsworth <m3k4e@byu.edu>
2025-02-19 16:23:35 -0700

    set up phase 3

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,30 @@
+package server;
+
+import spark.*;
+import handlers.RegisterHandler;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+
+        //This line initializes the server and can be removed once you have a functioning endpoint 
+        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+
+    public static void register(){
+        Spark.post("/user", new RegisterHandler());
+    }
+}
